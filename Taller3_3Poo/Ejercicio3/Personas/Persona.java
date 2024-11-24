b4be5714ebcb1a1aad668825733a06eb917eabcd
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-11-23 20:53:21 -0500

    Taller 3

---
+++ Taller3_3Poo/Ejercicio3/Personas/Persona.java
@@ -0,0 +1,15 @@
+package Taller3_3Poo.Ejercicio3.Personas;
+
+public class Persona {
+
+    private String nombre;
+    int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
