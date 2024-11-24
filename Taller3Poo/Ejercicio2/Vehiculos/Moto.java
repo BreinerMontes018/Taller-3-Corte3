3da9508745aa991c90999f1b23f6b24298e4cbce
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-11-23 20:52:54 -0500

    Taller 3

---
+++ Taller3Poo/Ejercicio2/Vehiculos/Moto.java
@@ -0,0 +1,13 @@
+package Taller3Poo.Ejercicio2.Vehiculos;
+
+class Moto extends Vehiculo {
+    
+    Moto(String tipo) {
+        super(tipo); 
+    }
+
+    void mostrarInfo() {
+        System.out.println("Esta es una moto.");
+        mostrarTipo(); 
+    }
+}
